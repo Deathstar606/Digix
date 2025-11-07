3a86432b8c0592627ec4ef824f33bc7e2b3c8c23
Deathstar <dsfardin606@gmail.com>
2025-11-07 17:53:56 +0600

    new back

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,6 +1,6 @@
 // baseUrl.js
 
-export const baseUrlProd = "https://digi-back-tau.vercel.app/";
+export const baseUrlProd = "https://digi-back-seven.vercel.app/";
 export const baseUrlDev = "http://localhost:9000/";
 
 // Choose which to use
